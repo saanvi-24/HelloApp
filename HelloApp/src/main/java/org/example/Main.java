b84338d69b0ab9a70830c8117f38274f51ff0f2d
saanvi-24 <saanvisonar2024@gmail.com>
2026-03-26 10:22:49 +0530

    UC3 Coded to Console

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 public class Main{
     public static void main( String[] args){
-        String name = args[0];
+        String name = "World";
+        if (args.length>0){
+            name=args[0];
+        }
         System.out.println("Hello, "+name+"!");
     }
 }
