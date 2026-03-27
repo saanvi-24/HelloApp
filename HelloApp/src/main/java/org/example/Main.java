6ae5c8635a65ab7078ac0bd79b814cf074ef53a1
saanvi-24 <saanvisonar2024@gmail.com>
2026-03-27 15:16:12 +0530

    UC5 Coded to Console

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -3,19 +3,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
 
-        if (args.length == 0) {
-            nameBuilder.append("World");
-        } else {
-            for (int i = 0; i < args.length; i++) {
-                nameBuilder.append(args[i]);
-                if (i < args.length - 1) {
-                    nameBuilder.append(", ");
-                }
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
             }
+            nameBuilder.append(name);
+            first = false;
         }
 
-        String name = nameBuilder.toString();
-        System.out.println("Hello " + name);
+        // Handle default case (no arguments)
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            System.out.println("Hello " + nameBuilder.toString());
+        }
     }
 }
